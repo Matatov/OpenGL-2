4043976aad21d1e6ff9411d855cba438ebc06269
Matatov <docmat63@gmail.com>
2022-03-06 12:01:34 +0200

    first commit

--- main.c
+++ main.c
@@ -1,114 +1,151 @@
-//lab01.c
+//lab02.c
 #include<stdio.h>
 #include<math.h>
-#include"GL/glut.h" // including GLUT library
+#include"GL/glut.h" 
 
 #define Pi 3.141592654
-#define POINTS_NUM 13 // The greater the number, the greater the density of points
-						
+
+//vertex array
 typedef struct POINT {
 	GLfloat x;
 	GLfloat y;
 } Point;
-//add new cooment
-void function1(float VecDeg, float VecSize, Point* p);
-float function2(float a);
-void drawCirlce(float radius);
-void drawingCB(void);
+#define POINTS_VERTEX_NUM 12 
+#define CIRCLE_VERTEX_NUM 72 
+Point points_vertexes[POINTS_VERTEX_NUM];
+Point circle_vertexes[CIRCLE_VERTEX_NUM];
 
-Point points[POINTS_NUM];
+//function declerations
+void Deg2Vector(float VecDeg, float VecSize, Point* p);
+float Deg2Rad(float deg);
+void drawingCB(void);
+void reshapeCB(int width, int height);
+
+
 
 int main(int argc, char* argv[])
 {
+	//initializing points and circle array
 	int n;
-	for (n = 0; n < POINTS_NUM-1; n++) 
-	{
-		function1((n + 1) * 30, 0.8, &points[n]);
+	for (n = 0; n < POINTS_VERTEX_NUM; n++) {
+		Deg2Vector((n + 1) * 360 / POINTS_VERTEX_NUM, 0.7, &points_vertexes[n]);
 	}
-	//the middle point
-	points[12].x = 0;
-	points[12].y = 0;
+	for (n = 0; n < CIRCLE_VERTEX_NUM; n++) {
+		Deg2Vector((n + 1) * 360 / CIRCLE_VERTEX_NUM, 0.9, &circle_vertexes[n]);
+	}
 
-	glutInit(&argc, argv);          //initialize the glut
-	glutInitWindowPosition(20, 20);	//set position	
-	glutInitWindowSize(200, 200);	//set size of window
-	glutInitDisplayMode(GLUT_RGB);  
+	//initizlizing GLUT
+	glutInit(&argc, argv);
 
+	//initializing window
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(200, 200);
+	glutInitDisplayMode(GLUT_RGB);
 	glutCreateWindow("Clock");
 
-	glutDisplayFunc(drawingCB);    //set the CallBack for Dysplat Event
-
+	//registering callbacks
+	glutReshapeFunc(reshapeCB);
+	glutDisplayFunc(drawingCB);
+	
+	//starting main loop
 	glutMainLoop();
 }
 
-void function1(float VecDeg, float VecSize, Point* p) // polarToCartesian
-{													
-	p->x = (GLfloat)cos(function2(VecDeg)) * VecSize;
-	p->y = (GLfloat)sin(function2(VecDeg)) * VecSize;
+// create vector from angle and size of vector
+void Deg2Vector(float VecDeg, float VecSize, Point* p)
+{
+	p->x = (GLfloat)cos(Deg2Rad(VecDeg)) * VecSize;
+	p->y = (GLfloat)sin(Deg2Rad(VecDeg)) * VecSize;
 }
 
-float function2(float a) // degreeToRadian(float a)
-{								
-	return a * Pi / 180.0;
+// convert degree to radians
+float Deg2Rad(float deg)
+{
+	return deg * Pi / 180.0;
 }
-/*
-*	This Functions create all objects
-*	in our window
-*/
-void drawingCB() 
+
+// rendering callback
+void drawingCB()
 {
 	int n;
 	GLenum er;
-	
-	//set color for backgoround
-	glClearColor(0.0f, 1.0f, 0.9f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	
-	//set color and size for points (Yellow)
-	glColor3f(1.0f, 1.0f, 0.0);
-	glPointSize(5.0f);
 
-	//Display points
+	//clearing the background
+	glClearColor(0.2f, 0.8f, 0.7f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//drawing points
+	glColor3f(0.1f, 0.5f, 0.1f);
+	glPointSize(4);
 	glBegin(GL_POINTS);
-	for (n = 0; n < POINTS_NUM; n++) {
-		glVertex2f(points[n].x, points[n].y);
+	for (n = 0; n < POINTS_VERTEX_NUM; n++) {
+		glVertex2f(points_vertexes[n].x, points_vertexes[n].y);
 	}
 	glEnd();
 
-	//Display clock hands
-	glLineWidth(2.0f);
-
-	glBegin(GL_LINES);
-		glColor3f(0.0, 0.0, 1.0f); 
-		//minute hand
-		glVertex2f(0.0f, 0.0f);
-		glVertex2f(0.0f, 0.7f);
-		//hour hand
-		glVertex2f(0.0f, 0.0f);
-		glVertex2f(0.5f, 0.0f);
+	//drawing circle
+	glLineWidth(2);
+	glColor3f(0.1f, 0.4f, 0.8f);
+	glBegin(GL_LINE_LOOP);
+	for (n = 0; n < CIRCLE_VERTEX_NUM; n++) {
+		glVertex2f(circle_vertexes[n].x, circle_vertexes[n].y);
+	}
 	glEnd();
 
+	//drawing clock lines
+	glColor3f(0.0f, 0.0f, 0.8f);
 	glBegin(GL_LINES);
-		drawCirlce(1.0f);
+	glVertex2f(0, 0);
+	glVertex2f(0.4, 0);
+	glVertex2f(0, 0);
+	glVertex2f(0, 0.6);
 	glEnd();
 
+	//drawing middle point
+	glColor3f(0.1f, 0.5f, 0.1f);
+	glPointSize(6);
+	glBegin(GL_POINTS);
+	glVertex2f(0, 0);
+	glEnd();
+
+	//forces execution of OpenGL functions in finite time
 	glFlush();
 
+	//check for errors
 	er = glGetError();  //get errors. 0 for no error, find the error codes in: https://www.opengl.org/wiki/OpenGL_Error
 	if (er) printf("error: %d\n", er);
-
 }
 
-void drawCirlce(float radius)
-{	
-	
-	Point point;
-	function1(360, radius, &point);
-	for (int j = 0; j < 360; j++)
+
+void reshapeCB(int width, int height)
+{
+	float left, right, bottom, top;
+	float AR;
+
+	// define our ortho
+	left = -1.5;
+	right = 1.5;
+	top = 1.5;
+	bottom = -1.5;
+
+	// 1)update viewport
+	glViewport(0, 0, width, height);
+	// 2) clear the transformation matrices (load identity)
+	glLoadIdentity();
+	// 3) compute the aspect ratio
+	AR = (float)width / height;
+	// 4) if AR>=1 update left, right
+	if (AR >= 1)
 	{
-		glVertex2f(point.x, point.y); //start point
-		function1(j, radius, &point);
-		glVertex2f(point.x, point.y); //finish point 
+		left = left * AR;
+		right = right * AR;
 	}
+	// 5) else i.e. AR<1 update the top, bottom
+	else
+	{
+		top = top / AR;
+		bottom = bottom / AR;
+	}
+	// 6) definnig the boundary of the model using gluOrtho2D 
+	gluOrtho2D(left, right, bottom, top);
 }
-
